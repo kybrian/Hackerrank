a7bdd893d45b436ea67a46232b621f512f9049f9
kybrian <kybrian.se@gmail.com>
2023-07-11 10:51:24 +0300

    Simple Problems

---
+++ Python/03_arithmeticOperators.py
@@ -0,0 +1,16 @@
+'''
+https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+'''
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+sum = a + b
+print(sum)
+
+difference = a - b
+print(difference)
+
+product = a * b
+print(product)
---
+++ Python/04_division.py
@@ -0,0 +1,13 @@
+'''
+https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true
+'''
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+intDivision = int(a / b)
+print(intDivision)
+
+floatDivision = a / b
+print(floatDivision)
