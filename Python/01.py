9ad2e8d1ec8041aad08b2bf895d57d921e666420
kybrian <kybrian.se@gmail.com>
2024-01-15 09:53:17 +0300

    02

--- Python/01.py
+++ Python/01.py
@@ -1,6 +1,5 @@
-'''
-https://www.hackerrank.com/challenges/py-hello-world/problem?isFullScreen=true
-'''
+# https://www.hackerrank.com/challenges/py-hello-world/problem?isFullScreen=true
 
 if __name__ == '__main__':
-    print("Hello, World!")
+    my_string = "Hello, World!"
+    print(my_string)
