e07359bb73a7a89a1bfd9909032008aaeb2bb121
kybrian <kybrian.se@gmail.com>
2024-01-15 09:54:03 +0300

    03

---
+++ Python/03.py
@@ -0,0 +1,14 @@
+# https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+sum = a + b
+print(sum)
+
+diff = a - b
+print(diff)
+
+prod = a * b
+print(prod)
