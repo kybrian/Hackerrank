e6667f73f81e18b62b50954d46c1182cdfbc7dcb
kybrian <kybrian.se@gmail.com>
2024-01-15 10:04:40 +0300

    04

---
+++ Python/04.py
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+int_div = int(a / b)
+print(int_div)
+
+float_div = float(a / b)
+print(float_div)
